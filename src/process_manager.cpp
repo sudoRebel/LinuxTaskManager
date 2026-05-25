77953963c3a7fb8c5e56047d205d7cc19e61681d
sudoRebel <mouad.scp.m@gmail.com>
2026-05-25 16:46:55 +0100

    Feat: Implement line-by-line streaming using std::getline in process manager

--- src/process_manager.cpp
+++ src/process_manager.cpp
@@ -1,4 +1,5 @@
 #include "process_manager.h"
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <dirent.h>
@@ -17,29 +18,50 @@ void procList() {
   }
 
   struct dirent *entry;
-  std::string pid, statusFilePath, word, vmRss, procName;
-  double kiloByte, bytes, megaByte, gigaByte, teraByte, petaByte;
 
   while ((entry = readdir(dir)) != NULL) {
+    double kiloByte;
+
     if (isdigit(entry->d_name[0])) {
-      pid = entry->d_name;
-      statusFilePath = "/proc/" + pid + "/status";
+      std::string pid = entry->d_name;
+      std::string statusFilePath = "/proc/" + pid + "/status";
 
       std::ifstream statusFile(statusFilePath);
-      if (!statusFile.is_open())
+
+      if (!statusFile.is_open()) {
         continue;
+      }
 
-      vmRss = "0";
-      procName = "Unknown";
+      std::string vmRss = "0";
+      std::string procName = "Unknown";
+      std::string line;
 
-      while (statusFile >> word) {
-        if (word == "Name:") {
-          statusFile >> procName;
-        } else if (word == "VmRSS:") {
-          statusFile >> vmRss;
-          break;
+      while (getline(statusFile, line)) {
+        size_t charIndexForName = line.find("Name:");
+
+        if (charIndexForName != std::string::npos) {
+          std::string rawName = line.substr(charIndexForName + 5);
+          size_t firstChar = rawName.find_first_not_of(" \t\r\n");
+          size_t lastChar = rawName.find_last_not_of(" \t\r\n");
+
+          if (firstChar != std::string::npos && lastChar != std::string::npos) {
+            procName = rawName.substr(firstChar, lastChar - (firstChar - 1));
+          }
+        }
+
+        size_t charIndexForVmRSS = line.find("VmRSS:");
+
+        if (charIndexForVmRSS != std::string::npos) {
+          std::string rawName = line.substr(charIndexForVmRSS + 6);
+          size_t firstChar = rawName.find_first_not_of(" \t\r\n");
+          size_t lastChar = rawName.find_last_not_of(" \t\r\n");
+
+          if (firstChar != std::string::npos && lastChar != std::string::npos) {
+            vmRss = rawName.substr(firstChar, lastChar - (firstChar - 1));
+          }
         }
       }
+      statusFile.close();
 
       try {
         kiloByte = stod(vmRss);
@@ -51,25 +73,23 @@ void procList() {
                 << " Name: " << std::setw(35) << procName << " RAM: ";
 
       if (kiloByte >= 1099511627776.0) {
-        petaByte = kiloByte / 1099511627776.0;
+        double petaByte = kiloByte / 1099511627776.0;
         std::cout << petaByte << " PB\n";
       } else if (kiloByte >= 1073741824.0) {
-        teraByte = kiloByte / 1073741824.0;
+        double teraByte = kiloByte / 1073741824.0;
         std::cout << teraByte << " TB\n";
       } else if (kiloByte >= 1048576.0) {
-        gigaByte = kiloByte / 1048576.0;
+        double gigaByte = kiloByte / 1048576.0;
         std::cout << gigaByte << " GB\n";
       } else if (kiloByte >= 1024.0) {
-        megaByte = kiloByte / 1024.0;
+        double megaByte = kiloByte / 1024.0;
         std::cout << megaByte << " MB\n";
       } else if (kiloByte >= 1.0) {
         std::cout << kiloByte << " kB\n";
       } else {
-        bytes = kiloByte * 1024.0;
+        double bytes = kiloByte * 1024.0;
         std::cout << bytes << " Byte\n";
       }
-
-      statusFile.close();
     }
   }
   closedir(dir);
